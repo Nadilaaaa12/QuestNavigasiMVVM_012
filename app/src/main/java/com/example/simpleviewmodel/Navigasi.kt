17d8b71fa38663d7afef0505e2674ef42a658478
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-11-21 16:14:31 +0700

    Kode ini menggabungkan layout utama (Scaffold), state management (uiState), dan navigasi (NavHost)

--- app/src/main/java/com/example/simpleviewmodel/Navigasi.kt
+++ app/src/main/java/com/example/simpleviewmodel/Navigasi.kt
@@ -1,10 +1,13 @@
 package com.example.simpleviewmodel
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.simpleviewmodel.viewmodel.MahasiswaViewModel
 
@@ -18,3 +21,13 @@ fun Navigasi(
     viewModel: MahasiswaViewModel = viewModel(),
     navHost: NavHostController = rememberNavController()
 ) {
+
+    Scaffold { isPadding ->
+        val uiState by viewModel.uiState.collectAsState()
+        NavHost(
+            modifier = modifier.padding(isPadding),
+            navController = navHost,
+            startDestination = Halaman.Form.name
+        ) {
+
+        }
