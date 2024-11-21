4a70501b111a826dd03da2fce1bf9ba9fb2e2873
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-11-21 22:02:40 +0700

    Menambahkan NIM,Memperbaiki error dan merubah posisi button

--- app/src/main/java/com/example/simpleviewmodel/model/Mahasiswa.kt
+++ app/src/main/java/com/example/simpleviewmodel/model/Mahasiswa.kt
@@ -2,6 +2,9 @@ package com.example.simpleviewmodel.model
 
 data class Mahasiswa(
     val nama: String = "",
+    val nim: String = "",
+    val email: String = "",
+    val noTelpon: String = "",
+    val alamat: String = "",
     val gender: String = "",
-    val alamat: String = ""
 )
