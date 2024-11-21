ea3c2c0606935c3e1de1d1539148178c1078ff51
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-11-21 17:18:39 +0700

    kode ini digunakan untuk memanggil halaman DetailMahasiswaView dan memanggil tombol back

--- app/src/main/java/com/example/simpleviewmodel/Navigasi.kt
+++ app/src/main/java/com/example/simpleviewmodel/Navigasi.kt
@@ -14,6 +14,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.simpleviewmodel.model.ListGender
+import com.example.simpleviewmodel.ui.theme.view.DetailMahasiswaView
 import com.example.simpleviewmodel.ui.theme.view.FormMahasiswaView
 import com.example.simpleviewmodel.viewmodel.MahasiswaViewModel
 
@@ -49,7 +50,12 @@ fun Navigasi(
                 )
             }
             composable(route = Halaman.Data.name){
-
+                DetailMahasiswaView(
+                    dataMhs = uiState,
+                    onBackButton = {
+                        navHost.popBackStack()
+                    }
+                )
             }
 
         }
