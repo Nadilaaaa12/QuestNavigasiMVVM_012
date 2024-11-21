9cb64a4518600e624682ad9210889add08519fc5
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-11-21 17:17:55 +0700

    menambahkan button back agar sesuai dengan module di myklass

--- app/src/main/java/com/example/simpleviewmodel/ui/theme/view/DetailMahasiswaView.kt
+++ app/src/main/java/com/example/simpleviewmodel/ui/theme/view/DetailMahasiswaView.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,6 +15,7 @@ import com.example.simpleviewmodel.model.Mahasiswa
 @Composable
 fun DetailMahasiswaView(
     dataMhs: Mahasiswa,
+    onBackButton: () -> Unit,
     modifier: Modifier = Modifier
 ){
     val listDataMhs = listOf(
@@ -31,6 +33,11 @@ fun DetailMahasiswaView(
             DetailMhs(judul = data.first,
                 isi = data.second)
         }
+    }
+    Button(
+        onClick = {onBackButton()}
+    ) {
+        Text(text = "Kembali")
     }
 }
 
