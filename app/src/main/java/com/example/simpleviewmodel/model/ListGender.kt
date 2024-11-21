022840b1d62a1c30619e5d7e101facb68bf89f09
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-11-21 16:56:41 +0700

    menambahkan variabel listJk dengan jenis kelamin laki dan wanita

--- app/src/main/java/com/example/simpleviewmodel/model/ListGender.kt
+++ app/src/main/java/com/example/simpleviewmodel/model/ListGender.kt
@@ -1,4 +1,10 @@
 package com.example.simpleviewmodel.model
 
+import com.example.simpleviewmodel.R
+
 object ListGender {
+    val listJk = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
 }
