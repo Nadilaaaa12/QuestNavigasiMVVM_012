9e809605dd02576df8f842d264d155d42b59d309
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-11-21 16:58:33 +0700

    kode ini digunakan untuk merepresentasikan suatu halaman

--- app/src/main/java/com/example/simpleviewmodel/Navigasi.kt
+++ app/src/main/java/com/example/simpleviewmodel/Navigasi.kt
@@ -3,12 +3,18 @@ package com.example.simpleviewmodel
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.simpleviewmodel.model.ListGender
+import com.example.simpleviewmodel.ui.theme.view.FormMahasiswaView
 import com.example.simpleviewmodel.viewmodel.MahasiswaViewModel
 
 enum class Halaman {
@@ -23,11 +29,16 @@ fun Navigasi(
 ) {
 
     Scaffold { isPadding ->
-        val uiState by viewModel.uiState.collectAsState()
+        val uiState by viewModel.dataModel.collectAsState()
         NavHost(
             modifier = modifier.padding(isPadding),
             navController = navHost,
             startDestination = Halaman.Form.name
         ) {
+            composable(route = Halaman.Form.name) {
+
+            }
 
         }
+    }
+}
